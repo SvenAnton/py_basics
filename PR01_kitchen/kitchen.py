c4cfde9e4a94f07bb0a7abcca5b405fbd989005b
svanto <svanto@ttu.ee>
2018-09-08 20:06:55 +0300

    version 01

---
+++ PR01_kitchen/kitchen.py
@@ -0,0 +1,13 @@
+print("Welcome to the kitchen.")
+name = input("What is your name?")
+print(f"Hi there, {name}!")
+drink = input("What do you like for a drink?")
+
+if drink == "tea":
+    print(f"Have a nice {drink}!")
+elif drink == "coffee":
+    print("Feeling tired?")
+else:
+    print("We only serve water.")
+
+

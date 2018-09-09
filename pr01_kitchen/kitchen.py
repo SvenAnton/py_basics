ed35bc4691a7df80c76920cba33b92c7dc66fb7c
svanto <svanto@ttu.ee>
2018-09-09 18:17:57 +0300

    PR01_kitchen esimene kodutöö

--- pr01_kitchen/kitchen.py
+++ pr01_kitchen/kitchen.py
@@ -7,4 +7,4 @@ if drink == "tea":  # kontrollin, kas kasutaja antud vastus oli "tea"
 elif drink == "coffee":  # kas vastus oli "coffee"?
     print("Feeling tired?")  # ja kui oli, siis prindin selle
 else:  # muudel juhtudel, kui ei ole ei "tea" ega "coffee"
-    print("We only serve water.")  # prindin muu versioon
+    print("We only serve water.")  # prindin muu versiooni

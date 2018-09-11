93323b5aca2072fedee487af42faf2377ef23aff
svanto <svanto@ttu.ee>
2018-09-11 19:48:57 +0300

    PR01_kitchen esimene kodutöö

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -1,4 +1,17 @@
-"""""See on süvapythoni Jänguru lahendus1"""
+"""""See on süvapythoni Jänguru lahendus1."""
+
+
+""""Lahenduseks on funktsioon, mis loob mõlema jänese teekonna listina, kus listi indeks on üks ajaühik t ja
+ja arvu suurus näitab distantsi kus viibitakse. t=0 on positsioon pärast esimest hüpet. Selleks olen teinud ka
+eraldi muutujad pos_after_jump1 ja pos_after_jump2. Listid olen praegu mugavuse mõttes jätnud x ja y, vastavalt siis
+1. jänes ja teine jänes. Olen arvutanud ka kiiruse selleks, et välistada olukorrad, kus kohtumist kunagi ei toimu.
+Kui jänese algpositsioon ja kiirus on mõlemad väiksemad kui teisel jänesel, siis ei leia kohtumine kunagi aset, sest
+teise hüppe ajaks on ka vahepealne magamisaeg läbitud.
+
+Kui olukord ei ole võimatu, siis hakkangi looma listi, kus kõrvutan järjest listi vastavaid elemente. Võrdlen esimest
+esimesega, teist teisega jne. Nii on tagatud olukord, kus jänesed on tõepoolest samal ajahetkel samal distantsil.
+
+Hetkel on kood väga ressurssimahukas ja ma proovin seda parandada"""
 
 def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
     pos_after_jump1 = pos1 + jump_distance1

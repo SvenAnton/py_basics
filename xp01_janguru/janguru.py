db81602be90c128a25d058a5aea2a53bb141fc33
svanto <svanto@ttu.ee>
2018-09-12 00:55:04 +0300

    syvapyyton

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -1,35 +1,26 @@
 """""See on süvapythoni Jänguru lahendus1."""
 
-""""Lahenduseks on funktsioon, mis loob mõlema jänese teekonna listina, kus listi indeks on üks ajaühik t ja
-ja arvu suurus näitab distantsi kus viibitakse. t=0 on positsioon pärast esimest hüpet. Selleks olen teinud ka
-eraldi muutujad pos_after_jump1 ja pos_after_jump2. Listid olen praegu mugavuse mõttes jätnud x ja y, vastavalt siis
-1. jänes ja teine jänes. Olen arvutanud ka kiiruse selleks, et välistada olukorrad, kus kohtumist kunagi ei toimu.
-Kui jänese algpositsioon ja kiirus on mõlemad väiksemad kui teisel jänesel, siis ei leia kohtumine kunagi aset, sest
-teise hüppe ajaks on ka vahepealne magamisaeg läbitud.
-
-Kui olukord ei ole võimatu, siis hakkangi looma listi, kus kõrvutan järjest listi vastavaid elemente. Võrdlen esimest
-esimesega, teist teisega jne. Nii on tagatud olukord, kus jänesed on tõepoolest samal ajahetkel samal distantsil.
-
-Hetkel on kood väga ressurssimahukas ja ma proovin seda parandada"""
-
 
 def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
-    """"See on funktsioon."""
-    speed1 = jump_distance1 / sleep1
+    """"Funktsioonis kasutan täiendavaid muutujaid kiiruse mõõtmiseks (speed1 ja speed2), while loop'ist
+    väljumiseks (exit) ja loop-korduste lugemiseks (count). Funktsiooni esimeses osas küsin if-funktsiooniga
+    võimatud olukorrad ja tagastan nende väärtuse -1. Teises osas jätkan võimalike juhtumitega ja alustan
+    kontrolli jäneste kohtumise osas. Kontrollin mõlemaid jäneseid korraga samadel ajahetkedel."""
+    speed1 = jump_distance1 / sleep1   # kiirus on liikumine ajahetke t kohta.
     speed2 = jump_distance2 / sleep2
     exit = 0
-    count = -1
-    pos1 = pos1 + jump_distance1
+    count = -1   # kordused alustan miinus ühest, sest siis on esimene kordus 0 ja vastab ajahetkele t0.
+    pos1 = pos1 + jump_distance1   # t0 on juba esimene hüpe toimunud
     pos2 = pos2 + jump_distance2
     if (pos1 < pos2 and speed1 <= speed2) or (pos2 < pos1 and speed2 <= speed1):
-        return -1
-    else:
-        while exit == 0:
-            if pos1 == pos2:
-                yield pos1
-                exit = 1
-            if (count % sleep1) == 0:
-                pos1 += jump_distance1
-            if (count % sleep2) == 0:
-                pos2 += jump_distance2
-            count += 1
+        return -1   # tagastan väärtuse, kui kohtumine on võimatu. Kohtumine on võimatu, kui mõlemad: nii
+    else:           # positsiooni kui ka kiiruse väärtused on väiksemad
+        while exit == 0:   # teen loop'i
+            if pos1 == pos2:   # kontrollin, kas positsioonid kattuvad,
+                return pos1    # tagastan positsiooni, kui jänesed on koos
+                exit = 1   # ja väljun loop'ist exit väärtust muutes.
+            if (count % sleep1) == 0:   # kui ei kattu, siis lisan positsiooni ajahetkel t2. Selleks
+                pos1 += jump_distance1  # kontrollin kõigepealt, kas magamise aeg on läbi. Magamise aeg on
+            if (count % sleep2) == 0:   # läbi siis, kui korduste jagub täpselt, st toimub momentaalne hüpe
+                pos2 += jump_distance2  # ja positsiooni väärtus suureneb.
+            count += 1   # korduste lugeja väärtus suureneb.

fe7e46e1089261b0db11b912fa284f6d0b61596e
svanto <svanto@ttu.ee>
2018-09-11 19:32:46 +0300

    Süvapython 1. lahendus

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -11,15 +11,14 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
     exit = 0
 
     if (pos1 < pos2 and speed1 <= speed2) or (pos2 < pos1 and speed2 <= speed1):
-        print(-1)
+        return -1
     else:
         while exit == 0:
             x.append(pos_after_jump1)
             y.append(pos_after_jump2)
             if pos_after_jump1 == pos_after_jump2:
-                print(pos_after_jump1)
+                return pos_after_jump1
                 exit = 1
-                break
             if (len(x) % sleep1) == 0:
                 pos_after_jump1 += jump_distance1
             if (len(y) % sleep2) == 0:

8801f119f68628a28b1dd8953d1866acb88a3896
svanto <svanto@ttu.ee>
2018-09-13 21:12:40 +0300

    Teine kodune töö

--- ex02_coordinates/polar.py
+++ ex02_coordinates/polar.py
@@ -7,27 +7,27 @@ from math import cos, sin, pi, atan, degrees, radians, sqrt
 def convert_polar_to_cartesian(r, phi):
     """"funktsioon, mis teeb polaarkoordinaadid
     ristkoordinaatideks."""
-    phi = radians(phi)
-    x = round(r*cos(phi), 2)
-    y = round(r*sin(phi),2)
-    return x, y
+    phi = radians(phi)   # teen fii radiaanideks
+    x = round(r * cos(phi), 2)  # arvutan x-i mat valemi järgi
+    y = round(r * sin(phi), 2)  # arvutan y-i mat valemi järgi
+    return (x, y) # tagastan x ja y koordinaadid ennikuna
 
 
 def convert_cartesian_to_polar(x, y):
     """Funktsioon, mis teen ristkoordinaadid
     polaarkoordinaatideks."""
-    r = round(sqrt(x*x + y*y), 2)
-    if x > 0:
-        phi = atan(y/x)
+    r = round(sqrt(x * x + y * y), 2)   # arvutan raadiuse Pythagorase teoreemi järgi
+    if x > 0:   # alustan x- ja y- koordinaatide kontrolli fii arvutamiseks
+        phi = atan(y / x)   # iga tingimuse järel on arvutus vastavalt mat tabelile
     elif x < 0 and y >= 0:
-        phi = atan(y/x) + pi
+        phi = atan(y / x) + pi
     elif x < 0 and y < 0:
-        phi = atan(y/x) - pi
+        phi = atan(y / x) - pi
     elif x == 0 and y > 0:
         phi = pi/2
     elif x == 0 and y < 0:
-        phi = -pi/2
+        phi = -pi / 2
     elif x == 0 and y == 0:
         phi = 0
-    phi = round(degrees(phi), 2)
-    return r, phi
+    phi = round(degrees(phi), 2)    # ümardan fii
+    return (r, phi)     # tagastan raadiuse ja fii ennikuna

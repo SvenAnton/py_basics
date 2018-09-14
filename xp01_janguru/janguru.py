9a2b36389b26521a074f298c090c1b5bdaa75ca6
svanto <svanto@ttu.ee>
2018-09-14 09:29:32 +0300

    Süvapy

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -5,34 +5,62 @@
 
 
 def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
-    """"Funktsiooni kasutan täiendavaid muutujaid.
 
-    Need on kiiruse mõõtmiseks (speed1 ja speed2), while loop'ist väljumiseks (exit) ja loop-korduste
-    lugemiseks (count). Funktsiooni esimeses osas küsin if-funktsiooniga võimatud olukorrad ja tagastan
-    nende väärtuse -1. Teises osas jätkan võimalike juhtumitega ja alustan kontrolli jäneste kohtumise osas.
-    Kontrollin mõlemaid jäneseid korraga samadel ajahetkedel.
-    """
     speed1 = jump_distance1 / sleep1   # kiirus on liikumine ajahetke t kohta.
     speed2 = jump_distance2 / sleep2
-    exit = 0
-    count = -1   # kordused alustan miinus ühest, sest siis on esimene kordus 0 ja vastab ajahetkele t0.
+    pos1 = pos1 + jump_distance1
+    pos2 = pos2 + jump_distance2
+    speed_differance = abs(speed1 - speed2)
+    if speed_differance == 0:
+        return -1
+    if pos1 == pos2:
+        return pos1
 
+    probable_meeting_time_minus_one = round(abs(pos1 - pos2) / abs(speed1 - speed2))
+
+
+    if probable_meeting_time_minus_one > ((2*sleep1) or (2*sleep2)):
+        if sleep1 < sleep2:
+            probable_meeting_time_minus_one = probable_meeting_time_minus_one - sleep2
+        else:
+            probable_meeting_time_minus_one = probable_meeting_time_minus_one - sleep1
+    else:
+        probable_meeting_time_minus_one = probable_meeting_time_minus_one - 1
+
+
+
+    pos1 = (probable_meeting_time_minus_one // sleep1) * jump_distance1 + pos1
+    pos2 = (probable_meeting_time_minus_one // sleep2) *jump_distance2 + pos2
+
+
+    to_sleep1 = sleep1 - (probable_meeting_time_minus_one % sleep1)
+    to_sleep2 = sleep2 - (probable_meeting_time_minus_one % sleep2)
+
+    #print("alustan arvutusi nendelt positsioonidelt: ", pos1, pos2)
+    #print(f"niipalju on magadaalgpositsioonilt: {to_sleep2}, {to_sleep1}")
+    #print("Võimalik kohtumisaeg miinus üks sleep: ", probable_meeting_time_minus_one)
+
+    exit = 0
+    count = 0
 
     if (pos1 < pos2 and speed1 <= speed2) or (pos2 < pos1 and speed2 <= speed1):
         return -1   # tagastan väärtuse, kui kohtumine on võimatu. Kohtumine on võimatu, kui mõlemad: nii
     else:
-        pos_difference = abs(pos1 - pos2)
-        speed_difference = abs(speed1 - speed2)
-        pos1 = round((pos_difference / speed_difference) * speed1 + pos1)
-        pos2 = round((pos_difference / speed_difference) * speed2 + pos2)
-        #print(pos1)
-        #print(pos2)
         while exit == 0:   # teen loop'i
+            #print(f"Esimene jänes: {pos1} [{to_sleep1}] Teine jänes:   {pos2} [{to_sleep2}] ")
+            #print(f" magada jäänud {sleep2 - to_sleep2}  magada jäänud: {sleep1 - to_sleep1}")
+
             if pos1 == pos2:   # kontrollin, kas positsioonid kattuvad,
                 return pos1    # tagastan positsiooni, kui jänesed on koos
                 exit = 1   # ja väljun loop'ist exit väärtust muutes.
-            if (count % sleep1) == 0:   # kui ei kattu, siis lisan positsiooni ajahetkel t2. Selleks
-                pos1 += jump_distance1  # kontrollin kõigepealt, kas magamise aeg on läbi. Magamise aeg on
-            if (count % sleep2) == 0:   # läbi siis, kui korduste jagub täpselt, st toimub momentaalne hüpe
-                pos2 += jump_distance2  # ja positsiooni väärtus suureneb.
-            count += 1   # korduste lugeja väärtus suureneb.
+
+            if to_sleep1 == 0:
+                pos1 += jump_distance1
+                to_sleep1 = sleep1
+
+            if to_sleep2 == 0:
+                pos2 += jump_distance2
+                to_sleep2 = sleep2
+
+            to_sleep1 -= 1
+            to_sleep2 -= 1

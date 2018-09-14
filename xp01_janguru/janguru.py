0bf8f070bfcfa5a0bccf9a0203f8f3ff11910964
svanto <svanto@ttu.ee>
2018-09-14 09:52:10 +0300

    Süvapy

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -1,43 +1,32 @@
 """""See on süvapythoni Jänguru lahendus1."""
 
 
-"""""See on süvapythoni Jänguru lahendus1."""
-
-
 def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
 
     speed1 = jump_distance1 / sleep1   # kiirus on liikumine ajahetke t kohta.
     speed2 = jump_distance2 / sleep2
     pos1 = pos1 + jump_distance1
     pos2 = pos2 + jump_distance2
-    speed_differance = abs(speed1 - speed2)
 
     if pos1 == pos2:
         return pos1
     elif jump_distance1 == 0 or jump_distance2 == 0:
-        #print("not jumping")
         return -1
-    elif speed_differance == 0:
-        #print("Ei liigu edasi")
+    elif (pos1 < pos2 and speed1 <= speed2) or (pos2 < pos1 and speed2 <= speed1):
         return -1
 
     probable_meeting_time_minus_one = round(abs(pos1 - pos2) / abs(speed1 - speed2))
 
-
     if probable_meeting_time_minus_one > ((2*sleep1) or (2*sleep2)):
         if sleep1 < sleep2:
-            probable_meeting_time_minus_one = probable_meeting_time_minus_one - sleep2
+            probable_meeting_time_minus_one = probable_meeting_time_minus_one - sleep2   # miinus sleep on selleks, et võtta toimiv pos tagasi
         else:
             probable_meeting_time_minus_one = probable_meeting_time_minus_one - sleep1
     else:
         probable_meeting_time_minus_one = probable_meeting_time_minus_one - 1
 
-
-
     pos1 = (probable_meeting_time_minus_one // sleep1) * jump_distance1 + pos1
     pos2 = (probable_meeting_time_minus_one // sleep2) *jump_distance2 + pos2
-
-
     to_sleep1 = sleep1 - (probable_meeting_time_minus_one % sleep1)
     to_sleep2 = sleep2 - (probable_meeting_time_minus_one % sleep2)
 
@@ -46,9 +35,6 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
     #print("Võimalik kohtumisaeg miinus üks sleep: ", probable_meeting_time_minus_one)
 
     exit = 0
-
-    if (pos1 < pos2 and speed1 <= speed2) or (pos2 < pos1 and speed2 <= speed1):
-        return -1   # tagastan väärtuse, kui kohtumine on võimatu. Kohtumine on võimatu, kui mõlemad: nii
 
     while exit == 0:   # teen loop'i
         #print(f"Esimene jänes: {pos1} [{to_sleep1}] Teine jänes:   {pos2} [{to_sleep2}] ")
@@ -67,3 +53,17 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
 
         to_sleep1 -= 1
         to_sleep2 -= 1
+
+test1 = meet_me(1, 2, 1, 2, 1, 1)   # 3
+test2 = meet_me(1, 2, 3, 4, 5, 5)   # -1
+test3 = meet_me(10, 7, 7, 5, 8, 6)  # 45
+test4 = meet_me(100, 7, 4, 300, 8, 6)   # 940
+test5 = meet_me(1, 7, 1, 15, 5, 1)   # 50
+test6 = meet_me(0, 1, 1, 1, 1, 1)   # -1
+
+print(test1)
+print(test2)
+print(test3)
+print(test4)
+print(test5)
+print(test6)

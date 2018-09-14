e92b0df4448cb4f2016872ef9025a9db42260cd3
svanto <svanto@ttu.ee>
2018-09-14 15:22:45 +0300

    Süvapy

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -5,6 +5,9 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
 
     speed1 = jump_distance1 / sleep1   # kiirus on liikumine ajahetke t kohta.
     speed2 = jump_distance2 / sleep2
+    start_pos1 = pos1 + jump_distance1
+    start_pos2 = pos2 + jump_distance2
+
     pos1 = pos1 + jump_distance1
     pos2 = pos2 + jump_distance2
 
@@ -30,15 +33,14 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
     to_sleep1 = sleep1 - (probable_meeting_time_minus_one % sleep1)
     to_sleep2 = sleep2 - (probable_meeting_time_minus_one % sleep2)
 
-    #print("alustan arvutusi nendelt positsioonidelt: ", pos1, pos2)
-    #print(f"niipalju on magadaalgpositsioonilt: {to_sleep2}, {to_sleep1}")
-    #print("Võimalik kohtumisaeg miinus üks sleep: ", probable_meeting_time_minus_one)
+    print("alustan arvutusi nendelt positsioonidelt: ", pos1, pos2)
+    print(f"niipalju on magadaalgpositsioonilt: {to_sleep2}, {to_sleep1}")
+    print("Võimalik kohtumisaeg miinus üks sleep: ", probable_meeting_time_minus_one)
 
     exit = 0
 
     while exit == 0:   # teen loop'i
-        #print(f"Esimene jänes: {pos1} [{to_sleep1}] Teine jänes:   {pos2} [{to_sleep2}] ")
-        #print(f" magada jäänud {sleep2 - to_sleep2}  magada jäänud: {sleep1 - to_sleep1}")
+        print(f"Esimene jänes: {pos1} [{to_sleep1}] Teine jänes:   {pos2} [{to_sleep2}] ")
 
         if pos1 == pos2:   # kontrollin, kas positsioonid kattuvad,
             return pos1    # tagastan positsiooni, kui jänesed on koos
@@ -54,16 +56,5 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
         to_sleep1 -= 1
         to_sleep2 -= 1
 
-test1 = meet_me(1, 2, 1, 2, 1, 1)   # 3
-test2 = meet_me(1, 2, 3, 4, 5, 5)   # -1
-test3 = meet_me(10, 7, 7, 5, 8, 6)  # 45
-test4 = meet_me(100, 7, 4, 300, 8, 6)   # 940
-test5 = meet_me(1, 7, 1, 15, 5, 1)   # 50
-test6 = meet_me(0, 1, 1, 1, 1, 1)   # -1
-
-print(test1)
-print(test2)
-print(test3)
-print(test4)
-print(test5)
-print(test6)
+        if (start_pos1 < start_pos2) and (pos2 < pos1) or (start_pos2 < start_pos1) and (pos1 < pos2):
+            return -1

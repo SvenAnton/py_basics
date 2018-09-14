2eee30926cfc3c3ad5da7bc01888761f1e40dba3
svanto <svanto@ttu.ee>
2018-09-14 23:11:03 +0300

    Süvapy

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -9,37 +9,43 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
     start_pos1 = pos1 + jump_distance1
     start_pos2 = pos2 + jump_distance2
 
-    pos1 = pos1 + jump_distance1
-    pos2 = pos2 + jump_distance2
 
-    if pos1 == pos2:
-        return pos1
-    elif jump_distance1 == 0 or jump_distance2 == 0:
+    if start_pos1 == start_pos2:
+        return start_pos1
+    elif (start_pos1 < start_pos2 and jump_distance1 == 0) or (start_pos2 < start_pos1 and jump_distance2 == 0):
         return -1
-    elif (pos1 < pos2 and speed1 <= speed2) or (pos2 < pos1 and speed2 <= speed1):
+    elif (start_pos1 < start_pos2 and speed1 <= speed2) or (start_pos2 < start_pos1 and speed2 <= speed1):
+        print("lootusetu püritus")
         return -1
 
-    probable_meeting_time_minus_one = round(abs(pos1 - pos2) / abs(speed1 - speed2))
 
-    if probable_meeting_time_minus_one > ((2*sleep1) or (2*sleep2)):
+    probable_meeting_place_minus = round(abs(start_pos1 - start_pos2) / abs(speed1 - speed2))
+
+    if probable_meeting_place_minus > ((2*sleep1) or (2*sleep2)):
         if sleep1 < sleep2:
-            probable_meeting_time_minus_one = probable_meeting_time_minus_one - sleep2   # miinus sleep on selleks, et võtta toimiv pos tagasi
+            probable_meeting_place_minus = probable_meeting_place_minus - sleep2*jump_distance2
         else:
-            probable_meeting_time_minus_one = probable_meeting_time_minus_one - sleep1
+            probable_meeting_place_minus = probable_meeting_place_minus - sleep1*jump_distance1
     else:
-        probable_meeting_time_minus_one = probable_meeting_time_minus_one - 1
+        probable_meeting_place_minus = probable_meeting_place_minus - 1
 
-    pos1 = (probable_meeting_time_minus_one // sleep1) * jump_distance1 + pos1
-    pos2 = (probable_meeting_time_minus_one // sleep2) *jump_distance2 + pos2
-    to_sleep1 = sleep1 - (probable_meeting_time_minus_one % sleep1)
-    to_sleep2 = sleep2 - (probable_meeting_time_minus_one % sleep2)
+    if probable_meeting_place_minus > 100 * abs(start_pos1 - start_pos2):
+        probable_meeting_place_minus = round(probable_meeting_place_minus / 7)
+        print("suur värk")
+
+    pos1 = (probable_meeting_place_minus // sleep1) * jump_distance1 + start_pos1
+    pos2 = (probable_meeting_place_minus // sleep2) *jump_distance2 + start_pos2
+    to_sleep1 = sleep1 - (probable_meeting_place_minus % sleep1)
+    to_sleep2 = sleep2 - (probable_meeting_place_minus % sleep2)
 
     #print("alustan arvutusi nendelt positsioonidelt: ", pos1, pos2)
-    #print(f"niipalju on magadaalgpositsioonilt: {to_sleep2}, {to_sleep1}")
-    #print("Võimalik kohtumisaeg miinus üks sleep: ", probable_meeting_time_minus_one)
+    #print(f"niipalju on magada algpositsioonilt: {to_sleep2}, {to_sleep1}")
+    #print("Võimalik kohtumiskoht miinus üks sleep: ", probable_meeting_place_minus)
+    #print(abs(speed1 - speed2), abs(start_pos1 - start_pos2))
+
 
     while True:   # teen loop'i
-        #print(f"Esimene jänes: {pos1} [{to_sleep1}] Teine jänes:   {pos2} [{to_sleep2}] ")
+        print(f"Esimene jänes: {pos1} [{to_sleep1}] Teine jänes:   {pos2} [{to_sleep2}] ")
 
         if pos1 == pos2:   # kontrollin, kas positsioonid kattuvad,
             return pos1    # tagastan positsiooni, kui jänesed on koos
@@ -54,6 +60,7 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
         to_sleep1 -= 1
         to_sleep2 -= 1
 
-        if (start_pos1 < start_pos2) and (pos2 < pos1) or (start_pos2 < start_pos1) and (pos1 < pos2):
-            if abs(pos1 - pos2) > (sleep1 or sleep2):
+        if ((start_pos1 < start_pos2) and (pos2 < pos1) or ((start_pos2 < start_pos1) and (pos1 < pos2))):
+            if abs(pos1 - pos2) > abs(start_pos1 - start_pos2):
+                #print("Hüppas mööda")
                 return -1

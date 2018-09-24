351d20a3f54093bc5e38e297f6a0108ce6f15f16
svanto <svanto@ttu.ee>
2018-09-24 21:00:13 +0300

    Süvapy

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -2,7 +2,23 @@
 
 
 def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
-    """"See on ülesande põhifuntksioon."""
+    """"See on ülesande põhifuntksioon.
+
+    Kasutan muutujaid lineaarse kiiruse arvtamiseks: speed1 ja speed2.
+    Samuti olen eraldi arvutanud stardipositsioonid välja: start_pos1 ja start_pos2.
+    Muutuja: probable_meeting_place_minus on lineaarse kiiruse põhjal arvestatud võimalik kohtumispaik,
+    millest olen lahutanud sleep'i ja võimaliku kohtumispaiga suhte järgi kas lihtsalt arvu 1 või
+    avaldise sleep*distance - seda selleks, et lineaarne kohtumispaik võib olla sõltuvalt sleepi
+    või distance'i tõttu oluliselt rohkem ees tegelikust kohtumispaigast. Korrutis võimaldab võtta
+    tagasi mõlemat muutujat arvestades, kuid kaotab oluliselt koodi kiirust.
+    Muutujad to_sleep on arvutus sellest, palju on jänestel jäänud magada kohas probable_meeting_place_minus.
+    Esimene if-klauslite rida välistab võimatud olukorrad ja välistab -1. Eelkontroll ei saada lootusetuid
+    juhtumeid loop'i.
+    Loop'is kontrollitakse iga korduse järel, kas positsioonid kattuvad. Kui ei, siis korratakse jäneste
+    liikumist arvestades nende magada jäänud päevi ja hüppe pikkust. Kui jänesel on veel magada vaja, siis
+    hüpet ei toimu. Loop'i sees on kontroll ka selle kohta, et ega kiirem jänes, kes alguses oli küll maas teisest,
+    ei ole vahepeal mööda läinud. Kui on, siis muutub kohtumine võimatuks ja väljastatakse -1.
+    """
     speed1 = jump_distance1 / sleep1   # kiirus on liikumine ajahetke t kohta.
     speed2 = jump_distance2 / sleep2
     start_pos1 = pos1 + jump_distance1
@@ -33,17 +49,14 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
     while True:
         if pos1 == pos2:
             return pos1
-
         if to_sleep1 == 0:
             pos1 += jump_distance1
             to_sleep1 = sleep1
         if to_sleep2 == 0:
             pos2 += jump_distance2
             to_sleep2 = sleep2
-
         to_sleep1 -= 1
         to_sleep2 -= 1
-
-        if (((start_pos1 < start_pos2) and (pos2 < pos1)) or ((start_pos2 < start_pos1) and (pos1 < pos2))):
+        if ((start_pos1 < start_pos2) and (pos2 < pos1)) or ((start_pos2 < start_pos1) and (pos1 < pos2)):
             if abs(pos1 - pos2) > sleep1 * (jump_distance1 or sleep2 * jump_distance2):
                 return -1

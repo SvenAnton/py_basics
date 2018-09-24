e62ea35a37590910dda29b6c287a3caa9f214e73
svanto <svanto@ttu.ee>
2018-09-24 21:38:55 +0300

    Süvapy

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -73,12 +73,3 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
         if ((start_pos1 < start_pos2) and (pos2 < pos1)) or ((start_pos2 < start_pos1) and (pos1 < pos2)):
             if abs(pos1 - pos2) > sleep1 * (jump_distance1 or sleep2 * jump_distance2):
                 return -1
-
-print(meet_me(1, 2, 1, 2, 1, 1)) #=> 3
-print(meet_me(1, 2, 3, 4, 5, 5)) #=> -1
-print(meet_me(10, 7, 7, 5, 8, 6)) #=> 45
-print(meet_me(100, 7, 4, 300, 8, 6)) #=> 940
-print(meet_me(1, 7, 1, 15, 5, 1)) #=> 50
-print(meet_me(0, 1, 1, 1, 1, 1)) # => -1
-
-

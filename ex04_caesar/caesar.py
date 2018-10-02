2565d74e7a7af3d8f4cc17b742c175523e6106c2
svanto <svanto@ttu.ee>
2018-10-02 20:45:36 +0300

    Süvapy

--- ex04_caesar/caesar.py
+++ ex04_caesar/caesar.py
@@ -10,7 +10,8 @@ def renumber(length: int, alphabet: str):
     else:
         return length
 
-def encode(message: str, shift: int, alphabet = "abcdefghijklmnopqrstuvwxyz") -> str:
+
+def encode(message: str, shift: int, alphabet="abcdefghijklmnopqrstuvwxyz"):
     """
     Encode the given message using the Caesar cipher principle.
 
@@ -24,15 +25,17 @@ def encode(message: str, shift: int, alphabet = "abcdefghijklmnopqrstuvwxyz") ->
     for i in range(len(message)):
         if message[i].upper() in alphabet.upper():
             if message[i].isupper():
-                message = message.replace(message[i], alphabet[renumber(alphabet.upper().find(message[i]) + shift, alphabet)].upper(), 1)
+                message = message.replace(message[i], alphabet[
+                    renumber(alphabet.upper().find(message[i]) + shift, alphabet)].upper(), 1)
             else:
-                message = message.replace(message[i], alphabet[renumber(alphabet.find(message[i]) + shift, alphabet)], 1)
+                message = message.replace(message[i], alphabet[renumber(alphabet.find(message[i]) + shift, alphabet)],
+                                          1)
         else:
             message
     return message
 
 
-def decode(message: str, shift: int, alphabet = "abcdefghijklmnopqrstuvwxyz") -> str:
+def decode(message: str, shift: int, alphabet="abcdefghijklmnopqrstuvwxyz"):
     """
     Decode the given message already encoded with the caesar cipher principle.
 

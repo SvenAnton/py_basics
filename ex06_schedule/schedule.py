d80d31d77ab978bf6da12ef4ff27998faf15f599
svanto <svanto@ttu.ee>
2018-10-20 19:16:29 +0300

    Süvapy

--- ex06_schedule/schedule.py
+++ ex06_schedule/schedule.py
@@ -18,7 +18,7 @@ def create_schedule_string(input_string: str) -> str:
     table = []
     schedule = {}
 
-    for match in re.finditer(r"( +|\n)([0-1]\d|[2][0-3]|\d)(\D)([0-5]\d|\d)( +)([a-zA-Z]+)", input_string):
+    for match in re.finditer(r"( +|\n)([0-1]\d|[2][0-3]|\d)(\D)([0-5]\d|\d)( +|\n +)([a-zA-Z]+)", input_string):
 
         if int(match.group(4)) < 10:
             minutes = f"0{int(match.group(4))}"
@@ -32,8 +32,8 @@ def create_schedule_string(input_string: str) -> str:
         else:
             time = f"{hour - 12:>2}:{minutes} PM"
 
-        schedule.setdefault(time, [])
-        schedule[time].append(match.group(6))
+        schedule.setdefault(time, set())
+        schedule[time].add(match.group(6).lower())
 
     if bool(schedule) is False:
         return(
@@ -61,9 +61,9 @@ def create_schedule_string(input_string: str) -> str:
     for time in sorted(schedule, key=lambda x: (x[6], x[0], x[1], x[3], x[4])):
         if int(time.split(":")[0]) == 0:
             table.append(
-                f"| {time.replace(' ', '', 1).replace('0', '12', 1)} | {', '.join(sorted(schedule[time])).lower():<{left_line}} |")
+                f"| {time.replace(' ', '', 1).replace('0', '12', 1)} | {', '.join(sorted(schedule[time])):<{left_line}} |")
         else:
-            table.append(f"| {time} | {', '.join(sorted(schedule[time])).lower():<{left_line}} |")
+            table.append(f"| {time} | {', '.join(sorted(schedule[time])):<{left_line}} |")
 
     # tabeli alumine serv
     table.append("-" * (left_line + 15))

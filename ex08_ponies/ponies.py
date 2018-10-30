60801231fdb336171779085bc9e89627b81a06b3
svanto <svanto@ttu.ee>
2018-10-30 23:45:29 +0200

    Süvapy

--- ex08_ponies/ponies.py
+++ ex08_ponies/ponies.py
@@ -1,4 +1,4 @@
-"""EX08"""
+"""EX08 homework."""
 
 
 import base64
@@ -6,7 +6,7 @@ import re
 
 
 def decode(line: str) -> str:
-    """Decodes given text to utf-8 format string type text."""
+    """Decode given text to utf-8 format string type text."""
     return str(base64.b64decode(line)).replace("b", "", 1).replace("'", "")
 
 
@@ -16,11 +16,11 @@ def extract_information(line: str) -> dict:
     pony_dict = {"name": " ".join(line.split(" ")[0:2]), "kind": str(line.split(" ")[2]),
                  "coat_color": str(line.split(" ")[3]), "mane_color": str(line.split(" ")[4]),
                  "eye_color": str(line.split(" ")[5]), "location": " ".join(line.split(" ")[6:])}
-    return  pony_dict
+    return pony_dict
 
 
 def read(read_file: str) -> list:
-    """Reads in given file, decodes lines and returns every line as a dictionary. Returns lines as a list."""
+    """Read in given file, decodes lines and returns every line as a dictionary. Returns lines as a list."""
     ponies_list = []
     try:
         with open(read_file, "r") as input_file:
@@ -31,26 +31,24 @@ def read(read_file: str) -> list:
                     continue
                 else:
                     ponies_list.append(extract_information(decode(line)))
-    except:
+    except FileNotFoundError:
         raise FileNotFoundError("File not found!")
-
-
 
     return ponies_list
 
 
 def filter_by_location(ponies: list) -> list:
-    """Filters out lines, where location is "None'."""
+    """Filter out lines, where location is "None'."""
     return [pony for pony in ponies if not (pony["location"] == "None")]
 
 
 def filter_by_kind(ponies: list, kind: str) -> list:
-    """Filters ponies from list by given "kind" value as a string."""
+    """Filter ponies from list by given "kind" value as a string."""
     return [pony for pony in ponies if (pony["kind"] == kind)]
 
 
 def get_points_for_color(color: str) -> int:
-    """Returns points by given color nr. First color gives 10 points, 2nd 9 etc. Points under 5 are returend as None"""
+    """Return points by given color nr. First color gives 10 points, 2nd 9 etc. Points under 5 are returend as None."""
     colors = [
         'magenta', 'pink', 'purple', 'orange', 'red', 'yellow', 'cyan', 'blue', 'brown', 'green'
     ]
@@ -64,18 +62,18 @@ def get_points_for_color(color: str) -> int:
 
 
 def add_points(pony: dict) -> dict:
-    """Evaluates ponies by location and returns new dictionary with points added"""
+    """Evaluate ponies by location and returns new dictionary with points added."""
     evaluation_locations = {
         'coat_color': ['Town Hall', 'Theater', 'School of Friendship'],
         'mane_color': ['Schoolhouse', 'Crusaders Clubhouse', 'Golden Oak Library'],
         'eye_color': ['Train station', 'Castle of Friendship', 'Retirement Village']
     }
-    if pony == None:
+    if pony is None:
         return None
     elif "location" not in pony.keys():
         return None
-    elif pony["location"] == None:
-        return None
+    elif pony["location"] is None:
+        pony["points"] = None
     else:
         if pony["location"] in evaluation_locations["coat_color"]:
             pony["points"] = get_points_for_color(pony["coat_color"])
@@ -90,20 +88,22 @@ def add_points(pony: dict) -> dict:
 
 
 def evaluate_ponies(ponies: list) -> list:
-    """Adds evaluation points to list on pony dictionary"""
+    """Adds evaluation points to list on pony dictionary."""
     for pony in ponies:
         add_points(pony)
     return ponies
 
 
 def sort_by_name(ponies: list) -> list:
+    """Sort ponies by their name."""
     return sorted(ponies, key=lambda pony: pony["name"])
 
 
 def sort_by_points(ponies: list) -> list:
+    """Sort ponies by their points."""
     ponies_list = []
     for pony in ponies:
-        if pony["points"] == None:
+        if pony["points"] is None:
             continue
         else:
             ponies_list.append(pony)
@@ -112,11 +112,13 @@ def sort_by_points(ponies: list) -> list:
 
 
 def format_line(pony: dict, place: int) -> str:
+    """Put pony directory in right format."""
     return f"{place:<10}{pony['points']:<10}{pony['name']:<20}{pony['kind']:<20}{pony['coat_color']:<20}" \
            f"{pony['mane_color']:<20}{pony['eye_color']:<20}{pony['location']}"
 
 
 def write(input_file: str, kind: str):
+    """Write correctly formatted pony-lines into a file."""
     place_table = "PLACE"
     points_table = "POINTS"
     name_table = "NAME"
@@ -134,27 +136,9 @@ def write(input_file: str, kind: str):
         ponies_string += f"{format_line(pony, place + 1)}\n"
     ponies_string = ponies_string.strip()
 
-
     with open(input_file, "r"):
         with open(f"results_for_{kind}.txt", "w") as output_file:
             output_file.write(f"{place_table:<10}{points_table:<10}{name_table:<20}{kind_table:<20}{coat_color:<20}"
                               f"{mane_color:<20}{eye_color:<20}{location}\n")
             output_file.write(f"{128 * line}\n")
             output_file.write(ponies_string)
-
-
-if __name__ == '__main__':
-    read("ponies1.txt")
-
-
-
-
-
-
-
-
-
-
-
-
-

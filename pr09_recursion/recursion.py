1ac10d18ce004f0feea3058cc4ff8ca86cd1ef1f
svanto <svanto@ttu.ee>
2018-11-05 16:02:33 +0200

    Proov

--- pr09_recursion/recursion.py
+++ pr09_recursion/recursion.py
@@ -1,13 +1,16 @@
+"""Recursion task."""
+
+
 def recursive_sum(numbers: list) -> int:
     """
     Find out the sum of all the even numbers using recursion.
+
     :param numbers: list of randomly ordered numbers
     :return: sum of even numbers
     """
     if len(numbers) == 1:
         return numbers[0] if (numbers[0] % 2) == 0 else 0
     return numbers[0] + recursive_sum(numbers[1:]) if (numbers[0] % 2) == 0 else 0 + recursive_sum(numbers[1:])
-
 
 
 def loop_sum(numbers: list) -> int:
@@ -38,13 +41,14 @@ def loop_reverse(s: str) -> str:
     return reversed_s
 
 
-
 def recursive_reverse(s: str) -> str:
     """Reverse a string using recursion.
 
     :param s: string
     :return: reverse of s
     """
+    if len(s) == 0:
+        return ""
     if len(s) == 1:
         return s
     return recursive_reverse(s[1:]) + s[0]

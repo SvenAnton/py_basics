cc275a60ad45cf260ce1cac69524d58a2cd98ece
svanto <svanto@ttu.ee>
2018-11-07 12:01:33 +0200

    Django p2rast teist tutorialit.

--- dj01_django_intro/dj01_django_intro/settings.py
+++ dj01_django_intro/dj01_django_intro/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,10 +77,13 @@ WSGI_APPLICATION = 'dj01_django_intro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'C:\ProgramData\MySQL\MySQL Server 8.0\my.ini',
+        },
     }
 }
+
 
 
 # Password validation
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Tallinn'
 
 USE_I18N = True
 
